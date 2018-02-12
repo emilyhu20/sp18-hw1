bc26a2c9554626c875e6d196e38e6d0334a81f83
Emily Hu <emilyhu20@berkeley.edu>
2018-02-11 17:30:31 -0800

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,12 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a = a.map{|x| x.to_i + 2}
+    a = a.select do |x|
+    	x%2 == 0 and x < 10
+    end
+    sum = 0
+    a.uniq.each{|x| sum += x}
+    return sum
   end
-end
+ end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a+b)*(a+b)
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  a.sort!
+  a = a.map {|x| x+1}
+  return a
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + " " + last_name
 
 end
 
@@ -49,4 +50,5 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  word.chars.map {|el| el = values[:"#{el}"]}.sum
 end
